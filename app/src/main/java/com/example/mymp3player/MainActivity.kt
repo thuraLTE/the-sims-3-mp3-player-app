deece9bec25d4db963294c9fe517022fde951e93
Thura <sean.ventus17th@gmail.com>
2023-04-06 08:04:24 +0700

    Integrate Foreground Service

--- app/src/main/java/com/example/mymp3player/MainActivity.kt
+++ app/src/main/java/com/example/mymp3player/MainActivity.kt
@@ -1,44 +1,46 @@
 package com.example.mymp3player
 
-import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var intent: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        initializeMediaPlayerInstance()
 
         val playButton = findViewById<Button>(R.id.play_btn)
         val pauseButton = findViewById<Button>(R.id.pause_btn)
         val stopButton = findViewById<Button>(R.id.stop_btn)
 
         playButton.setOnClickListener {
-            mediaPlayer.start()
-            mediaPlayer.setOnCompletionListener {
-                Toast.makeText(this, "Track $it is done!", Toast.LENGTH_SHORT).show()
-            }
+            sendCommandToService(Constants.ACTION_PLAY_MUSIC)
         }
 
         pauseButton.setOnClickListener {
-            mediaPlayer.pause()
+            sendCommandToService(Constants.ACTION_PAUSE_MUSIC)
         }
 
         stopButton.setOnClickListener {
-            mediaPlayer.reset()
-            initializeMediaPlayerInstance()
+            sendCommandToService(Constants.ACTION_STOP_MUSIC)
         }
     }
 
-    private fun initializeMediaPlayerInstance() {
-        mediaPlayer = MediaPlayer.create(this, R.raw.the_sims_3_main_theme)
-        mediaPlayer.setVolume(0.5f, 0.5f)
+    private fun sendCommandToService(action: String): Intent {
+        intent = Intent(this, MusicPlayingService::class.java)
+            .also { intent ->
+                intent.action = action
+                startService(intent)
+            }
+        return intent
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopService(intent)
     }
 }
